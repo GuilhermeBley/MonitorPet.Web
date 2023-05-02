83d570f57b67459e416aa4de9f7487e776847d40
Guilherme Bley <guilhermecbley@gmail.com>
2023-05-01 22:49:53 -0300

    Model 'Consumption' #19

---
+++ src/MonitorPet.Application/Model/PesoHistorico/ConsumptionModel.cs
@@ -0,0 +1,7 @@
+﻿namespace MonitorPet.Application.Model.PesoHistorico;
+
+public class ConsumptionModel
+{
+    public DateTimeOffset Date { get; set; }
+    public decimal QttConsumption { get; set; }
+}
