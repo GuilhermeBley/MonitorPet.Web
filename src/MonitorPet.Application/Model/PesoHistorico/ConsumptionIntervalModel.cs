dcd4a3413ab3bd97d043f8ce4df4989929ba5b42
Guilherme Bley <guilhermecbley@gmail.com>
2023-05-01 22:55:30 -0300

    Model 'ConsumptionInterval' #20

---
+++ src/MonitorPet.Application/Model/PesoHistorico/ConsumptionIntervalModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MonitorPet.Application.Model.PesoHistorico;
+
+public class ConsumptionIntervalModel
+{
+    public DateTimeOffset Start { get; set; }
+    public DateTimeOffset End { get; set; }
+    public IReadOnlyList<ConsumptionModel> Consumptions { get; set; }
+        = new List<ConsumptionModel>();
+    public DateTimeOffset GeneratedAt { get; set; }
+}
