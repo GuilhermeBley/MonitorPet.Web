27582d12b0e58b94ad0c253b8d1e424de2112b0b
Guilherme Bley <guilhermecbley@gmail.com>
2023-05-06 01:50:03 -0300

    Ui - Charts #24

--- src/MonitorPet.Ui/Client/Program.cs
+++ src/MonitorPet.Ui/Client/Program.cs
@@ -2,12 +2,16 @@ using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MonitorPet.Ui.Client;
+using System.Globalization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pt-BR");
+CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("pt-BR");
 
 builder.Services.AddScoped<MonitorPet.Ui.Client.Services.LocalStorageService>();
 builder.Services.AddScoped<MonitorPet.Ui.Client.Services.JwtTokenService>();
