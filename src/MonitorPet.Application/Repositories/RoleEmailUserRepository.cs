7b1b020b801f4a259953022c962e73d78dd5bac9
GuilhermeBley <guilhermecbley@gmail.com>
2023-05-21 17:05:25 -0300

    Repository RoleUserEmail #30

---
+++ src/MonitorPet.Application/Repositories/RoleEmailUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using MonitorPet.Application.Model.Email;
+using MonitorPet.Core.Entity;
+
+namespace MonitorPet.Application.Repositories;
+
+public interface RoleEmailUserRepository : IRepositoryBase<RoleEmailUser, RoleEmailUserModel, int>
+{
+    IEnumerable<QueryRoleEmailUserModel> GetByIdUser(int idUser);
+}
