0aee4b2e88cfe1e60a2a925277c30c52785aad71
Guilherme Bley <guilhermecbley@gmail.com>
2023-05-22 00:03:31 -0300

    Infrastructure - Implement repository #78

---
+++ src/MonitorPet.Infrastructure/Repositories/RoleEmailUserRepository.cs
@@ -0,0 +1,75 @@
+﻿using Dapper;
+using MonitorPet.Application.Model.Email;
+using MonitorPet.Application.Repositories;
+using MonitorPet.Core.Entity;
+using MonitorPet.Infrastructure.UoW;
+using static Dapper.SqlMapper;
+
+namespace MonitorPet.Infrastructure.Repositories;
+
+internal class RoleEmailUserRepository : RepositoryBase, IRoleEmailUserRepository
+{
+    public RoleEmailUserRepository(MySqlSession mysqlSession) : base(mysqlSession)
+    {
+    }
+
+    public async Task<RoleEmailUserModel> Create(RoleEmailUser entity)
+        => await _connection.QueryFirstOrDefaultAsync<RoleEmailUserModel>(
+            @"INSERT INTO monitorpet.regraemailuser (UserId, EmailTypeId) VALUES (@UserId, @EmailTypeId);"+
+            @"SELECT Id Id, IdUsuario UserId, IdTipoEmail EmailTypeId FROM monitorpet.regraemailuser;
+WHERE Id=last_insert_id();",
+            new { UserId = entity.UserId, EmailTypeId = entity.Role.Id },
+            transaction: _transaction
+        );
+
+    public async Task<RoleEmailUserModel?> DeleteByIdOrDefault(int id)
+        => await _connection.QueryFirstOrDefaultAsync<RoleEmailUserModel>(
+            @"SELECT Id Id, IdUsuario UserId, IdTipoEmail EmailTypeId FROM monitorpet.regraemailuser;
+WHERE Id=@Id;" +
+            @"DELETE FROM monitorpet.regraemailuser WHERE Id=@Id;",
+            new { Id = id },
+            transaction: _transaction
+        );
+
+    public async Task<int> DeleteByIdUser(int idUser)
+        => await _connection.ExecuteAsync(
+                @"DELETE FROM monitorpet.regraemailuser WHERE IdUsuario=@IdUsuario;",
+                new { IdUsuario = idUser },
+                transaction: _transaction
+            );
+
+    public Task<IEnumerable<RoleEmailUserModel>> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<RoleEmailUserModel?> GetByIdOrDefault(int id)
+        => await _connection.QueryFirstOrDefaultAsync<RoleEmailUserModel>(
+            @"SELECT Id Id, IdUsuario UserId, IdTipoEmail EmailTypeId FROM monitorpet.regraemailuser;
+WHERE Id=@Id;",
+            new { Id = id },
+            transaction: _transaction
+        );
+
+    public async Task<IEnumerable<QueryRoleEmailUserModel>> GetByIdUser(int idUser)
+        => await _connection.QueryAsync<QueryRoleEmailUserModel>(
+                @"
+SELECT 
+	Id Id, 
+    IdUsuario UserId, 
+    IdTipoEmail EmailTypeId,
+    TipoEnvio Type, 
+    Descricao Description
+FROM monitorpet.regraemailuser r
+INNER JOIN monitorpet_tipoemail t
+	ON r.IdTipoEmail = t.Id;
+WHERE IdUsuario=@IdUsuario;",
+                new { IdUsuario = idUser },
+                transaction: _transaction
+            );
+
+    public Task<RoleEmailUserModel?> UpdateByIdOrDefault(int id, RoleEmailUser entity)
+    {
+        throw new NotImplementedException();
+    }
+}
